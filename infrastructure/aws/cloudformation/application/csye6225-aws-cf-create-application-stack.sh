da75cc6c20bffb717f50f1b33bd67ef16198e514
Poojithshetty <shetty.poo@northeastern.edu>
2018-10-20 22:03:22 -0400

    travis

--- infrastructure/aws/cloudformation/application/csye6225-aws-cf-create-application-stack.sh
+++ infrastructure/aws/cloudformation/application/csye6225-aws-cf-create-application-stack.sh
@@ -2,14 +2,13 @@
 echo "Creating Stack"
 stack_name=$1
 keyTagValue=$2
-ec2InstanceTagValue=$3
-s3BucketTagValue=$4
+s3BucketTagValue=$3
 echo $stack_name
 
 webSecurityGroupTagValue=csye6225-webapp
 dbSecurityGroupTagValue=csye6225-rds
 
-stackId=$(aws cloudformation create-stack --stack-name $stack_name --template-body file://csye6225-cf-application.json --parameters ParameterKey=ec2InstanceTag,ParameterValue=$ec2InstanceTagValue ParameterKey=webSecurityGroupTag,ParameterValue=$webSecurityGroupTagValue ParameterKey=dbSecurityGroupTag,ParameterValue=$dbSecurityGroupTagValue ParameterKey=keyTag,ParameterValue=$keyTagValue ParameterKey=s3BucketTag,ParameterValue=$s3BucketTagValue --query [StackId] --capabilities CAPABILITY_NAMED_IAM --output text)
+stackId=$(aws cloudformation create-stack --stack-name $stack_name --template-body file://csye6225-cf-application.json --parameters ParameterKey=webSecurityGroupTag,ParameterValue=$webSecurityGroupTagValue ParameterKey=dbSecurityGroupTag,ParameterValue=$dbSecurityGroupTagValue ParameterKey=keyTag,ParameterValue=$keyTagValue ParameterKey=s3BucketTag,ParameterValue=$s3BucketTagValue --query [StackId] --capabilities CAPABILITY_NAMED_IAM --output text)
 
 echo $stackId
 
