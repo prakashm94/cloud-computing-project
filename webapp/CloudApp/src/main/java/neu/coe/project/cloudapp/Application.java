b76a4632b2cbfe167e952d4a99b6cb347b298192
Poojithshetty <shetty.poo@northeastern.edu>
2018-10-23 19:49:10 -0400

    Adding springbootservletinitializer

--- webapp/CloudApp/src/main/java/neu/coe/project/cloudapp/Application.java
+++ webapp/CloudApp/src/main/java/neu/coe/project/cloudapp/Application.java
@@ -2,7 +2,9 @@ package neu.coe.project.cloudapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
@@ -10,7 +12,13 @@ import org.springframework.context.annotation.ComponentScan;
 //        FileStorageProperties.class
 //})
 
-public class Application {
+public class Application extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(
+            SpringApplicationBuilder builder) {
+        return builder.sources(Application.class);
+    }
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
