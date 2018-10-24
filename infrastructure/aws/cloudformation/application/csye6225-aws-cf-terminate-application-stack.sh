95755069840b787f514efb46cb1099e65358b5f6
Vrushali Shah <shah.vru@husky.neu.edu>
2018-10-24 18:33:00 -0400

    updated application terminate stack script and json

--- infrastructure/aws/cloudformation/application/csye6225-aws-cf-terminate-application-stack.sh
+++ infrastructure/aws/cloudformation/application/csye6225-aws-cf-terminate-application-stack.sh
@@ -2,14 +2,13 @@
 echo "Updating Stack To Delete"
 stack_name=$1
 keyTagValue=$2
-ec2InstanceTagValue=$3
-s3BucketTagValue=$4
+s3BucketTagValue=$3
 echo $stack_name
 
 webSecurityGroupTagValue=csye6225-webapp
 dbSecurityGroupTagValue=csye6225-rds
 
-aws cloudformation update-stack --stack-name $stack_name --template-body file://csye6225-cf-application-terminate.json --parameters ParameterKey=ec2InstanceTag,ParameterValue=$ec2InstanceTagValue ParameterKey=webSecurityGroupTag,ParameterValue=$webSecurityGroupTagValue ParameterKey=dbSecurityGroupTag,ParameterValue=$dbSecurityGroupTagValue ParameterKey=keyTag,ParameterValue=$keyTagValue ParameterKey=s3BucketTag,ParameterValue=$s3BucketTagValue 
+aws cloudformation update-stack --stack-name $stack_name --template-body file://csye6225-cf-application-terminate.json --parameters ParameterKey=webSecurityGroupTag,ParameterValue=$webSecurityGroupTagValue ParameterKey=dbSecurityGroupTag,ParameterValue=$dbSecurityGroupTagValue ParameterKey=keyTag,ParameterValue=$keyTagValue ParameterKey=s3BucketTag,ParameterValue=$s3BucketTagValue 
 
 aws cloudformation wait stack-update-complete --stack-name $stack_name
 echo "STACK UPDATED SUCCESSFULLY"
