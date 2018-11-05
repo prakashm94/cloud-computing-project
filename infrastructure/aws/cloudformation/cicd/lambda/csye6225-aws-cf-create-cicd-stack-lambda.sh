08e65f76705852ccedc3ba728d8b415a184244f2
Vrushali Shah <shah.vru@husky.neu.edu>
2018-11-04 23:32:10 -0500

    working on lambda function and modified sns topic

---
+++ infrastructure/aws/cloudformation/cicd/lambda/csye6225-aws-cf-create-cicd-stack-lambda.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+echo "Creating CICD Stack"
+
+stack_name=$1
+domain_name=$2
+applicationName=$3
+applicationNameLambda=$4 
+#application lambda=password_reset
+snsTopicName=$5
+
+export account_id=$(aws sts get-caller-identity --query "Account" --output text)
+export region=us-east-1
+
+export resource1="arn:aws:codedeploy:"$region":"$account_id":application:"$applicationName
+export resource2="arn:aws:codedeploy:"$region":"$account_id":deploymentconfig:CodeDeployDefault.OneAtATime"
+export resource3="arn:aws:codedeploy:"$region":"$account_id":deploymentconfig:CodeDeployDefault.HalfAtATime"
+export resource4="arn:aws:codedeploy:"$region":"$account_id":deploymentconfig:CodeDeployDefault.AllAtOnce"
+export resource5="arn:aws:s3:::code-deploy."$domain_name"/"
+#export resource6="arn:aws:sns:"$region":"$account_id":$applicationNameLambda
+export resource6="arn:aws:sns:"$region":"$account_id":"$snsTopicName
+
+stackId=$(aws cloudformation create-stack --stack-name $stack_name --template-body file://csye6225-cf-cicd-lambda.json --capabilities CAPABILITY_NAMED_IAM --parameters ParameterKey=resource1,ParameterValue=$resource1 ParameterKey=resource2,ParameterValue=$resource2 ParameterKey=resource3,ParameterValue=$resource3 ParameterKey=resource4,ParameterValue=$resource4 ParameterKey=resource5,ParameterValue=$resource5 ParameterKey=domainName,ParameterValue=$domain_name ParameterKey=applicationName,ParameterValue=$applicationName ParameterKey=applicationNameLambda,ParameterValue=$applicationNameLambda ParameterKey=resource6,ParameterValue=$resource6 ParameterKey=TopicName,ParameterValue=$snsTopicName --query [StackId] --output text)
+
+echo '#############################'
+echo $stackId
+echo '#############################'
+
+if [ -z $stackId ]; then
+    echo 'Error occurred.Dont proceed. TERMINATED'
+else
+    aws cloudformation wait stack-create-complete --stack-name $stack_name
+    echo 'STACK CREATION COMPLETE.'
+fi
