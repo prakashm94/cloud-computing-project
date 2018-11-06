2875860ef59b485a4bd0f67414516edcb961f20c
prashantreddy069 <reddy.pra@northeastern.edu>
2018-11-06 02:39:47 -0500

    sns

--- webapp/CloudApp/src/main/java/neu/coe/project/cloudapp/Controllers/LoginController.java
+++ webapp/CloudApp/src/main/java/neu/coe/project/cloudapp/Controllers/LoginController.java
@@ -1,9 +1,11 @@
 package neu.coe.project.cloudapp.Controllers;
 
+import com.amazonaws.services.sns.model.PublishResult;
 import neu.coe.project.cloudapp.Model.UserData;
 import neu.coe.project.cloudapp.Repository.UserDataRepository;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,7 +15,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-
+import com.google.gson.JsonObject;
+//import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+//import com.amazonaws.services.dynamodbv2.datamodeling.*;
+//import com.amazonaws.services.sns.AmazonSNSClient;
+import com.amazonaws.services.sns.AmazonSNSClient;
+import com.amazonaws.services.sns.model.PublishRequest;
 import java.nio.charset.StandardCharsets;
 import java.text.SimpleDateFormat;
 import java.util.Base64;
@@ -27,7 +34,8 @@ import static org.springframework.web.bind.annotation.RequestMethod.POST;
 @RequestMapping
 public class LoginController {
     private static int workload = 17;
-
+    @Value("${aws.acccoutnId}") String accountId;
+    @Value("${aws.topicName}") String password_reset;
 
     public String[] retrieveParameters(String authorization) {
         String[] values = {};
@@ -165,6 +173,55 @@ public class LoginController {
     Iterable<UserData> getAllUsers() {
         // This returns a JSON or XML with the users
         return userDataRepository.findAll();
+    }
+
+    @RequestMapping(path="user/reset",method=POST)
+    public @ResponseBody
+    ResponseEntity<String> resetPassword (@RequestHeader HttpHeaders httpRequest) {
+
+        final String authorization = httpRequest.getFirst("Authorization");
+
+        String[] values = retrieveParameters(authorization);
+        String username = values[0];
+        String password = values[1];
+        Map<String, String> map = new HashMap<String, String>();
+
+        if (isValidEmailAddress(username)) {
+
+            Iterable<UserData> allusers = userDataRepository.findAll();
+            for (UserData user : allusers) {
+                if (user.equals(username)) {
+                    AmazonSNSClient snsClient = new AmazonSNSClient();
+//            AmazonSNSClient snsClient;
+//            snsClient= AmazonSNSClientBuilder
+//                    .standard()
+//                    .withCredentials(new AWSStaticCredentialsProvider())
+//                    .build();
+
+                    JsonObject jsonObject = new JsonObject();
+                    jsonObject.addProperty("username", username);
+                    PublishRequest emailPublishRequest = new PublishRequest("accoundId" + accountId, ":" +password_reset, jsonObject.toString());
+                    PublishResult emailPublishResult = snsClient.publish(emailPublishRequest);
+
+                    return ResponseEntity
+                            .status(HttpStatus.OK)
+                            .body("User " + username + " created successfully");
+                } else {
+                    return ResponseEntity
+                            .status(HttpStatus.BAD_REQUEST)
+                            .body("invalid user");
+                }
+            }
+        }
+        else{
+//                map.put("message", "Username already exists");
+//                return new JSONObject(map).toString();
+            return ResponseEntity
+                    .status(HttpStatus.BAD_REQUEST)
+                    .body("not a valid user");
+        }
+
+        return null;
     }
 
     public String hashPassword(String password_plaintext) {
