f9ea276ef64ab72cb5f4a4f58b0516e748ea747d
Poojithshetty <shetty.poo@northeastern.edu>
2018-11-14 18:00:53 -0500

    statsd success

--- infrastructure/aws/cloudformation/serverless/csye6225-aws-cf-create-serverless-stack.sh
+++ infrastructure/aws/cloudformation/serverless/csye6225-aws-cf-create-serverless-stack.sh
@@ -12,7 +12,7 @@ export region=us-east-1
 export resource1="arn:aws:iam::"$account_id":role/LambdaExecutionRole"
 
 
-stackId=$(aws cloudformation create-stack --stack-name $stack_name --template-body file://csye6225-cf-newserverless.json --parameters ParameterKey=topicName,ParameterValue=$topicName ParameterKey=domain,ParameterValue=$domain ParameterKey=resource1,ParameterValue=$resource1 --query [StackId] --capabilities CAPABILITY_NAMED_IAM --output text)
+stackId=$(aws cloudformation create-stack --stack-name $stack_name --template-body file://csye6225-cf-serverless.json --parameters ParameterKey=topicName,ParameterValue=$topicName ParameterKey=domain,ParameterValue=$domain ParameterKey=resource1,ParameterValue=$resource1 --query [StackId] --capabilities CAPABILITY_NAMED_IAM --output text)
 
 #stackId=$(aws cloudformation create-stack --stack-name $stack_name --template-body file://csye6225-cf-newserverless.json --parameters ParameterKey=resource1,ParameterValue=$resource1 --query [StackId] --capabilities CAPABILITY_NAMED_IAM --output text)
 
