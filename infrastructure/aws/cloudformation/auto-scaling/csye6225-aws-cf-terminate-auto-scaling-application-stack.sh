13678674b4b594d80d5ab2b71e7eb997b9d067b5
Poojithshetty <shetty.poo@northeastern.edu>
2018-11-25 16:03:23 -0500

    Adding files without app

--- infrastructure/aws/cloudformation/auto-scaling/csye6225-aws-cf-terminate-auto-scaling-application-stack.sh
+++ infrastructure/aws/cloudformation/auto-scaling/csye6225-aws-cf-terminate-auto-scaling-application-stack.sh
@@ -30,11 +30,6 @@ aws s3 rm $bucket_base_path$s3BucketLambda --recursive
 aws s3 rb $bucket_base_path$s3BucketLambda --force
 
 
-aws cloudformation update-stack --stack-name $stack_name --template-body file://csye6225-cf-application-terminate.json  --capabilities CAPABILITY_NAMED_IAM --parameters ParameterKey=webSecurityGroupTag,ParameterValue=$webSecurityGroupTagValue ParameterKey=dbSecurityGroupTag,ParameterValue=$dbSecurityGroupTagValue ParameterKey=keyTag,ParameterValue=$keyTagValue ParameterKey=s3BucketCloud,ParameterValue=$s3BucketCloud ParameterKey=DBUSER,ParameterValue=$DBUSER ParameterKey=DBPWD,ParameterValue=$DBPWD ParameterKey=s3BucketLambda,ParameterValue=$s3BucketLambda ParameterKey=topicName,ParameterValue=$topicName ParameterKey=elbSecurityGroupNameTag,ParameterValue=$elb_security_group ParameterKey=domainName,ParameterValue=$domainName ParameterKey=hostedZoneName,ParameterValue=$domainName$endPeriod
-
-aws cloudformation wait stack-update-complete --stack-name $stack_name
-echo "STACK UPDATED SUCCESSFULLY"
-
 aws cloudformation delete-stack --stack-name $stack_name
 aws cloudformation wait stack-delete-complete --stack-name $stack_name
 
